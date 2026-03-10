d490bf40bed30239d55a201aa5ad8952a1da7113
Francis-play <93499490+Francis-play@users.noreply.github.com>
2026-03-09 22:30:54 -0400

    Use BuildConfig for architecture and Hermes settings

--- android/app/src/main/java/com/pagohelper/MainApplication.kt
+++ android/app/src/main/java/com/pagohelper/MainApplication.kt
@@ -20,8 +20,8 @@ class MainApplication : Application(), ReactApplication {
 
       override fun getJSMainModuleName(): String = "index"
 
-      override val isNewArchEnabled: Boolean = false
-      override val isHermesEnabled: Boolean = true
+      override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+      override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
     }
 
   override fun onCreate() {
