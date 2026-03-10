41aa24205040a4d8f5df5bf2fd28dd3e2692871a
Francis-play <93499490+Francis-play@users.noreply.github.com>
2026-03-09 20:04:04 -0400

    Refactor MainApplication to use ReactNativeHost

--- android/app/src/main/java/com/pagohelper/MainApplication.kt
+++ android/app/src/main/java/com/pagohelper/MainApplication.kt
@@ -3,25 +3,34 @@ package com.pagohelper
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
-import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactHost: ReactHost by lazy {
-    getDefaultReactHost(
-      context = applicationContext,
-      packageList =
-        PackageList(this).packages.apply {
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // add(MyReactNativePackage())
-        },
-    )
+  private val mReactNativeHost: ReactNativeHost = object : ReactNativeHost(this) {
+
+    override fun getUseDeveloperSupport(): Boolean {
+      return BuildConfig.DEBUG
+    }
+
+    override fun getPackages(): List<ReactPackage> {
+      val packages = PackageList(this).packages
+      return packages
+    }
+
+    override fun getJSMainModuleName(): String {
+      return "index"
+    }
+  }
+
+  override fun getReactNativeHost(): ReactNativeHost {
+    return mReactNativeHost
   }
 
   override fun onCreate() {
     super.onCreate()
-    loadReactNative(this)
+    SoLoader.init(this, false)
   }
 }
